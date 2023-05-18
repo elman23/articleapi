12b6f301050e38cfd7b522b2e002dcd8d797ce6a
Elia Manara <e.manara90@gmail.com>
2023-05-18 09:06:55 -0500

    created REST API methods

---
+++ pkg/handlers/deleteArticle.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/elman23/articleapi/pkg/mocks"
+	"github.com/gorilla/mux"
+)
+
+func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range mocks.Articles {
+		if article.Id == id {
+			mocks.Articles = append(mocks.Articles[:index], mocks.Articles[index+1:]...)
+
+			w.Header().Add("Content-type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Deleted")
+			break
+		}
+	}
+}
---
+++ pkg/handlers/updateArticle.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/elman23/articleapi/pkg/mocks"
+	"github.com/elman23/articleapi/pkg/models"
+	"github.com/gorilla/mux"
+)
+
+func UpdateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// Read the request body
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var updatedArticle models.Article
+	json.Unmarshal(body, &updatedArticle)
+
+	for index, article := range mocks.Articles {
+		if article.Id == id {
+			article.Title = updatedArticle.Title
+			article.Desc = updatedArticle.Desc
+			article.Content = updatedArticle.Content
+
+			mocks.Articles[index] = article
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Updated")
+			break
+		}
+	}
+}
